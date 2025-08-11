699c493f4c6fa9ca6bd969f143f7f2630324af2b
trli <potter@trli.club>
2025-08-11 19:50:51 +0800

    update

--- pkg/logger/otel/exporters.go
+++ pkg/logger/otel/exporters.go
@@ -8,7 +8,7 @@ import (
 	"go.opentelemetry.io/otel/exporters/otlp/otlplog/otlploghttp"
 	"go.opentelemetry.io/otel/sdk/log"
 
-	"github.com/zPotterli20/golem/pkg/logger"
+	"github.com/Potterli20/golem/pkg/logger"
 )
 
 // createExporter creates the appropriate OTLP exporter based on protocol
